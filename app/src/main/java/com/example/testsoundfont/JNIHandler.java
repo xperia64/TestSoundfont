c04ed051dc5ea1a45934690811cdfbd993c24c20
xperia64 <xperiancedapps@gmail.com>
2016-03-15 20:29:44 -0400

    Fix playback

--- app/src/main/java/com/example/testsoundfont/JNIHandler.java
+++ app/src/main/java/com/example/testsoundfont/JNIHandler.java
@@ -286,8 +286,8 @@ public class JNIHandler {
         }
 
         Logger.i("song");
-        loadSongTimidity(songTitle);
         instance.track.play();
+		loadSongTimidity(songTitle);
         Logger.i("welcome play");
 
         return 0;
